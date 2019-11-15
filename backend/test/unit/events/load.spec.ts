2896872e08d535883f9a49802bf396f002619ede
Ben New <ben@leftclick.com.au>
2019-11-15 11:53:45 +0800

    Tests for `events/load` and `events/save` modules.

---
+++ backend/test/unit/events/load.spec.ts
@@ -0,0 +1,209 @@
+import { SinonStub, stub } from 'sinon';
+import { expect } from 'chai';
+import mockedEnv from 'mocked-env';
+import { DynamoDB } from 'aws-sdk';
+import { GameEvent } from '../../../src/events/types';
+import { loadEvents } from '../../../src/events/load';
+
+describe('Loading events from the event store', () => {
+  describe('Given the environment is correctly configured', () => {
+    let restoreEnv: () => void;
+
+    beforeEach(() => {
+      restoreEnv = mockedEnv({
+        DB_TABLE_EVENTS: 'events_unit_test'
+      })
+    });
+
+    afterEach(() => {
+      restoreEnv();
+    });
+
+    describe('Given the event store is returning a single page of results', () => {
+      let queryStub: SinonStub;
+      let documentClientStub: SinonStub;
+
+      beforeEach(() => {
+        queryStub = stub().returns({
+          promise: stub().resolves({
+            Items: [
+              'first event',
+              'second event',
+              'third event'
+            ]
+          })
+        });
+
+        documentClientStub = stub(DynamoDB, 'DocumentClient').returns({ query: queryStub });
+      });
+
+      afterEach(() => {
+        documentClientStub.restore();
+      });
+
+      describe('When invoked', () => {
+        let result: GameEvent[];
+
+        beforeEach(async () => {
+          result = await loadEvents('game-42');
+        });
+
+        it('Queries the database once', () => {
+          expect(queryStub.calledOnce).to.equal(true);
+          expect(queryStub.firstCall.args).to.deep.equal([
+            {
+              TableName: 'events_unit_test',
+              ExclusiveStartKey: undefined,
+              KeyConditionExpression: '#key = :key',
+              ExpressionAttributeNames: {
+                '#key': 'gameId'
+              },
+              ExpressionAttributeValues: {
+                ':key': 'game-42'
+              }
+            }
+          ]);
+        });
+
+        it('Returns the correct results', () => {
+          expect(result).to.deep.equal(['first event', 'second event', 'third event']);
+        });
+      });
+    });
+
+    describe('Given the event store is returning multiple pages of results', () => {
+      let queryStub: SinonStub;
+      let documentClientStub: SinonStub;
+
+      beforeEach(() => {
+        queryStub = stub().returns({
+          promise: stub()
+            .onFirstCall().resolves({
+              Items: [
+                'first event',
+                'second event',
+                'third event'
+              ],
+              LastEvaluatedKey: 'there-are-more-results'
+            })
+            .onSecondCall().resolves({
+              Items: [
+                'fourth event',
+                'fifth event',
+                'sixth event'
+              ],
+              LastEvaluatedKey: 'still-more-results'
+            })
+            .onThirdCall().resolves({
+              Items: [
+                'final event'
+              ]
+            })
+        });
+
+        documentClientStub = stub(DynamoDB, 'DocumentClient').returns({ query: queryStub });
+      });
+
+      afterEach(() => {
+        documentClientStub.restore();
+      });
+
+      describe('When invoked', () => {
+        let result: GameEvent[];
+
+        beforeEach(async () => {
+          result = await loadEvents('game-42');
+        });
+
+        it('Queries the database once per page', () => {
+          expect(queryStub.calledThrice).to.equal(true);
+          expect(queryStub.firstCall.args).to.deep.equal([
+            {
+              TableName: 'events_unit_test',
+              ExclusiveStartKey: undefined,
+              KeyConditionExpression: '#key = :key',
+              ExpressionAttributeNames: {
+                '#key': 'gameId'
+              },
+              ExpressionAttributeValues: {
+                ':key': 'game-42'
+              }
+            }
+          ]);
+          expect(queryStub.secondCall.args).to.deep.equal([
+            {
+              TableName: 'events_unit_test',
+              ExclusiveStartKey: 'there-are-more-results',
+              KeyConditionExpression: '#key = :key',
+              ExpressionAttributeNames: {
+                '#key': 'gameId'
+              },
+              ExpressionAttributeValues: {
+                ':key': 'game-42'
+              }
+            }
+          ]);
+          expect(queryStub.thirdCall.args).to.deep.equal([
+            {
+              TableName: 'events_unit_test',
+              ExclusiveStartKey: 'still-more-results',
+              KeyConditionExpression: '#key = :key',
+              ExpressionAttributeNames: {
+                '#key': 'gameId'
+              },
+              ExpressionAttributeValues: {
+                ':key': 'game-42'
+              }
+            }
+          ]);
+        });
+
+        it('Returns the correct results', () => {
+          expect(result).to.deep.equal([
+            'first event', 'second event', 'third event', 'fourth event', 'fifth event', 'sixth event', 'final event'
+          ]);
+        });
+      });
+    });
+
+    describe('Given the event store is causing errors', () => {
+      const thrownError = Error('Database failure code 666');
+      let queryStub: SinonStub;
+      let documentClientStub: SinonStub;
+
+      beforeEach(() => {
+        queryStub = stub().returns({
+          promise: stub().rejects(thrownError)
+        });
+
+        documentClientStub = stub(DynamoDB, 'DocumentClient').returns({ query: queryStub });
+      });
+
+      afterEach(() => {
+        documentClientStub.restore();
+      });
+
+      describe('When invoked', () => {
+        it('Throws the database error', async () => {
+          await expect(loadEvents('game-42'))
+            .to.be.eventually.rejectedWith(thrownError);
+
+          expect(queryStub.calledOnce).to.equal(true);
+          expect(queryStub.firstCall.args).to.deep.equal([
+            {
+              TableName: 'events_unit_test',
+              ExclusiveStartKey: undefined,
+              KeyConditionExpression: '#key = :key',
+              ExpressionAttributeNames: {
+                '#key': 'gameId'
+              },
+              ExpressionAttributeValues: {
+                ':key': 'game-42'
+              }
+            }
+          ]);
+        });
+      });
+    });
+  });
+});
---
+++ backend/test/unit/events/save.spec.ts
@@ -0,0 +1,123 @@
+import { SinonStub, stub } from 'sinon';
+import { expect } from 'chai';
+import mockedEnv from 'mocked-env';
+import { DynamoDB } from 'aws-sdk';
+import { saveEvent } from '../../../src/events/save';
+import { GameEvent, GameEventType } from '../../../src/events/types';
+import * as idModule from '../../../src/id';
+
+describe('Saving an event to the event store', () => {
+  let dateNowStub: SinonStub;
+  let generateIdStub: SinonStub;
+
+  beforeEach(() => {
+    dateNowStub = stub(Date, 'now').returns(1573726643625);
+    generateIdStub = stub(idModule, 'generateId').returns('generated-cuid');
+  });
+
+  afterEach(() => {
+    dateNowStub.restore();
+    generateIdStub.restore();
+  });
+
+  describe('Given the environment is correctly configured', () => {
+    let restoreEnv: () => void;
+
+    beforeEach(() => {
+      restoreEnv = mockedEnv({
+        DB_TABLE_EVENTS: 'events_unit_test'
+      })
+    });
+
+    afterEach(() => {
+      restoreEnv();
+    });
+
+    describe('Given the event store handles save operations normally', () => {
+      let putStub: SinonStub;
+      let documentClientStub: SinonStub;
+
+      beforeEach(() => {
+        putStub = stub().returns({
+          promise: stub().resolves()
+        });
+
+        documentClientStub = stub(DynamoDB, 'DocumentClient').returns({ put: putStub });
+      });
+
+      afterEach(() => {
+        documentClientStub.restore();
+      });
+
+      describe('When invoked', () => {
+        let event: GameEvent;
+
+        beforeEach(async () => {
+          event = await saveEvent(GameEventType.stockDealtToWaste, { gameId: 'game-42' });
+        });
+
+        it('Puts the item in the database', () => {
+          expect(putStub.callCount).to.equal(1);
+          expect(putStub.firstCall.args).to.deep.equal([
+            {
+              TableName: 'events_unit_test',
+              Item: {
+                eventId: 'generated-cuid',
+                eventTimestamp: 1573726643625,
+                eventType: 'stockDealtToWaste',
+                gameId: 'game-42'
+              }
+            }
+          ]);
+        });
+
+        it('Returns the created event', () => {
+          expect(event).to.deep.equal({
+            eventId: 'generated-cuid',
+            eventTimestamp: 1573726643625,
+            eventType: 'stockDealtToWaste',
+            gameId: 'game-42'
+          });
+        });
+      });
+    });
+
+    describe('Given the event store is causing errors', () => {
+      const thrownError = Error('Database failure code 666');
+      let putStub: SinonStub;
+      let documentClientStub: SinonStub;
+
+      beforeEach(() => {
+        putStub = stub().returns({
+          promise: stub().rejects(thrownError)
+        });
+
+        documentClientStub = stub(DynamoDB, 'DocumentClient').returns({ put: putStub });
+      });
+
+      afterEach(() => {
+        documentClientStub.restore();
+      });
+
+      describe('When invoked', () => {
+        it('Throws the database error', async () => {
+          await expect(saveEvent(GameEventType.stockDealtToWaste, { gameId: 'game-42' }))
+            .to.be.eventually.rejectedWith(thrownError);
+
+          expect(putStub.calledOnce).to.equal(true);
+          expect(putStub.firstCall.args).to.deep.equal([
+            {
+              TableName: 'events_unit_test',
+              Item: {
+                eventId: 'generated-cuid',
+                eventTimestamp: 1573726643625,
+                eventType: 'stockDealtToWaste',
+                gameId: 'game-42'
+              }
+            }
+          ]);
+        });
+      });
+    });
+  });
+});
