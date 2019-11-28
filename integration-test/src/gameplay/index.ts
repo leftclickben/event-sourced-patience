2342f216b657fc7295e2a5c79bde0f8b094e0f5a
Ben New <ben@leftclick.com.au>
2019-11-28 20:23:01 +0800

    Remove unnecessary `{ await ... }`

--- integration-test/src/gameplay/index.ts
+++ integration-test/src/gameplay/index.ts
@@ -1,11 +1,10 @@
 import { movesByGame } from './moves';
 
-export const playGames = async (apiBaseUrl: string): Promise<void> => {
-  await Object.keys(movesByGame).reduce(
+export const playGames = async (apiBaseUrl: string): Promise<void> =>
+  Object.keys(movesByGame).reduce(
     async (promise, gameId) => {
       await promise;
       console.log(`Playing game "${gameId}" at API base URL "${apiBaseUrl}"`);
     },
     Promise.resolve()
   );
-};
