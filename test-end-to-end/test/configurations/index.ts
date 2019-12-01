46b91e1229fb5684e61e0735141268a9ec782b2d
Ben New <ben@leftclick.com.au>
2019-12-01 18:11:57 +0800

    Add `newGamePlayToVictory` test.

--- test-end-to-end/test/configurations/index.ts
+++ test-end-to-end/test/configurations/index.ts
@@ -3,10 +3,12 @@ import { newGameToForfeit } from './newGameToForfeit';
 import { gameReadyToClaimVictory } from './gameReadyToClaimVictory';
 import { newGameToShowHelpAndAttemptVictory } from './newGameToShowHelpAndAttemptVictory';
 import { newGameToMakeSomeMoves } from './newGameToMakeSomeMoves';
+import { newGamePlayToVictory } from './newGamePlayToVictory';
 
 export const testConfigurations: Record<GameId, TestConfigurationBuilder> = {
   newGameToForfeit,
   gameReadyToClaimVictory,
   newGameToShowHelpAndAttemptVictory,
-  newGameToMakeSomeMoves
+  newGameToMakeSomeMoves,
+  newGamePlayToVictory
 };
