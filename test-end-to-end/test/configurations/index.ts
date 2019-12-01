508315afc14cd7ab9f43cf73397b6d1b99aeea84
Ben New <ben@leftclick.com.au>
2019-12-01 19:57:19 +0800

    Improve e2d integration test error handling example.

--- test-end-to-end/test/configurations/index.ts
+++ test-end-to-end/test/configurations/index.ts
@@ -1,14 +1,14 @@
 import { GameId, TestConfigurationBuilder } from '../../src/types';
 import { newGameToForfeit } from './newGameToForfeit';
 import { gameReadyToClaimVictory } from './gameReadyToClaimVictory';
-import { newGameToShowHelpAndAttemptVictory } from './newGameToShowHelpAndAttemptVictory';
 import { newGameToMakeSomeMoves } from './newGameToMakeSomeMoves';
 import { newGamePlayToVictory } from './newGamePlayToVictory';
+import { newGameForInvalidCommandsAndHelp } from './newGameForInvalidCommandsAndHelp';
 
 export const testConfigurations: Record<GameId, TestConfigurationBuilder> = {
   newGameToForfeit,
   gameReadyToClaimVictory,
-  newGameToShowHelpAndAttemptVictory,
   newGameToMakeSomeMoves,
-  newGamePlayToVictory
+  newGamePlayToVictory,
+  newGameForInvalidCommandsAndHelp
 };
