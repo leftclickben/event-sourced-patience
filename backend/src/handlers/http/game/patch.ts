ac01ec40e56fda9f58ad7b3e14c564c623668065
Ben New <ben@leftclick.com.au>
2020-06-08 21:45:04 +0800

    Rename vars for readability (#25)
    * Some readability improvements
    
    + Rename variables mentioning `delegations` to what it actually is, `commandProcessors`
    + Move what is now `getCommandProcessor` to its own module in the right place, simplifying the handler
    + Change some spacing

--- backend/src/handlers/http/game/patch.ts
+++ backend/src/handlers/http/game/patch.ts
@@ -2,38 +2,18 @@ import { loadEvents } from '../../../events/load';
 import { buildTableState } from '../../../state/table';
 import { buildScoreState } from '../../../state/score';
 import { GameplayCommandName } from '../../../commands/types';
-import { playWasteToTableau } from '../../../commands/processors/playWasteToTableau';
-import { playTableauToTableau } from '../../../commands/processors/playTableauToTableau';
-import { playTableauToFoundation } from '../../../commands/processors/playTableauToFoundation';
-import { playWasteToFoundation } from '../../../commands/processors/playWasteToFoundation';
-import { dealStockToWaste } from '../../../commands/processors/dealStockToWaste';
-import { resetWasteToStock } from '../../../commands/processors/resetWasteToStock';
-import { claimVictory } from '../../../commands/processors/claimVictory';
+import { getCommandProcessor } from '../../../commands/processors';
 import { APIGatewayProxyHandlerWithData, checkArguments, checkEnvironment, wrapHttpHandler } from '../helpers';
-
-const getDelegation = (key: GameplayCommandName) => ({
-  dealStockToWaste,
-  resetWasteToStock,
-  playWasteToTableau,
-  playWasteToFoundation,
-  playTableauToFoundation,
-  playTableauToTableau,
-  claimVictory
-}[key]);
 
 export const patchGameHandler: APIGatewayProxyHandlerWithData = async ({ data, pathParameters }) => {
   checkEnvironment(['DB_TABLE_EVENTS']);
 
   const { gameId, moveType } = pathParameters;
-
   checkArguments({ gameId, moveType });
 
-  const delegation = getDelegation(moveType as GameplayCommandName);
-
+  const commandProcessor = getCommandProcessor(moveType as GameplayCommandName);
   const events = await loadEvents(gameId);
-
-  const addedEvent = await delegation({ gameId, ...data as any });
-
+  const addedEvent = await commandProcessor({ gameId, ...data as any });
   const newEvents = [...events, addedEvent];
 
   const { score, status } = buildScoreState(newEvents);
