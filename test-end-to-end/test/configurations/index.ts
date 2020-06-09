fcab02107c37fb15085decb994c312f0bdbf87fa
Ben New <ben@leftclick.com.au>
2020-06-09 23:39:11 +0800

    Allow "claim victory" to be issued earlier (#27)
    Allow "claim victory" to be issued earlier
    
    + Previously, all the foundations had to be complete before victory could be claimed
    + Now, the waste and stock must be empty and the tableau must consist of only face up cards
    * Add new end-to-end test with early victory+

--- test-end-to-end/test/configurations/index.ts
+++ test-end-to-end/test/configurations/index.ts
@@ -4,11 +4,13 @@ import { gameReadyToClaimVictory } from './gameReadyToClaimVictory';
 import { newGameToMakeSomeMoves } from './newGameToMakeSomeMoves';
 import { newGamePlayToVictory } from './newGamePlayToVictory';
 import { newGameForInvalidCommandsAndHelp } from './newGameForInvalidCommandsAndHelp';
+import { newGamePlayToVictoryClaimedEarly } from './newGamePlayToVictoryClaimedEarly';
 
 export const testConfigurations: Record<GameId, TestConfigurationBuilder> = {
   newGameToForfeit,
   gameReadyToClaimVictory,
   newGameToMakeSomeMoves,
   newGamePlayToVictory,
+  newGamePlayToVictoryClaimedEarly,
   newGameForInvalidCommandsAndHelp
 };
